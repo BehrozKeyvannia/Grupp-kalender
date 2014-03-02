e59dbfe62dfd983be2ac527ffb98476fe7d18771
RamiJabor <ramijabor@gmail.com>
2014-03-02 17:40:28 +0100

    Update EventManager.java

--- Grupp-kalender/Engine/EventManager.java
+++ Grupp-kalender/Engine/EventManager.java
@@ -30,7 +30,7 @@ public class EventManager extends Observable {
 
 	/**
 	 * 
-	 * @return The events list of all events
+	 * @return The eventList
 	 */
 	public LinkedList<Event> getEventList() {
 		return eventList;
@@ -49,10 +49,10 @@ public class EventManager extends Observable {
 	}
 
 	/**
-	 * Adds an event to a user's Event list
+	 * Adds all the events from a list to eventList
 	 * 
-	 * @param event
-	 *            The event that will be added
+	 * @param list
+	 *            The list of events that will be added
 	 */
 	public void addEventList(LinkedList<Event> list) {
 		Iterator<Event> tempIterator = list.iterator();
@@ -60,11 +60,20 @@ public class EventManager extends Observable {
 			addEvent(tempIterator.next());
 		}
 	}
-
+	/**
+	* Sorts all events in Eventlist in chronlogical order.
+	*/
 	public void sortEventList() {
 		Collections.sort(eventList);
 	}
-
+	
+	/**
+	 *
+	 * Adds a event if it isn't already in eventList and adds the event in all the participants list in hMap. Notifies Observers
+	 * @param event
+	 * 		The event that will be added
+	 * 
+	 */
 	public Event addEvent(Event event) {
 		if (!eventList.contains(event)) {
 			eventList.addLast(event);
@@ -95,11 +104,24 @@ public class EventManager extends Observable {
 		return searchEvent(hMap.get(user), hashCode);
 
 	}
-
+	
+	/**
+	 * Searches for an Event in eventList and returnes the event with the specified hashCode
+	 * @param hashCode hashCode to be searched
+	 * @return Event If Event found it is returned else returns null.
+	 * 
+	 */
+	
 	public Event getEventByID(int hashCode) {
 		return searchEvent(eventList, hashCode);
 	}
-
+	/**
+	 * Searches through a list(param) for a Event with the specified hashCode(param)
+	 * @param list List to be searched
+	 * @param hashCode hashCode to search after.
+	 * 
+	 * @return Event if found else null.
+	 */
 	private Event searchEvent(LinkedList<Event> list, int hashCode) {
 		Event tempEvent;
 		Iterator<Event> tempIterator = list.iterator();
@@ -111,13 +133,22 @@ public class EventManager extends Observable {
 		// Inget event hittas returnas null
 		return null;
 	}
-
+	/**
+	 * Removes the EventReference with the specified hashCode in a user list and removes the users name in the event's userNames.
+	 * @param user user to remove
+	 * @param hashCode Events hashcode which the user is removed from.
+	 * 
+	 */
 	public void removeUserFromEvent(String user, int hashCode) {
 		if (!hMap.containsKey(user))
 			if (hMap.get(user).remove(getEvent(user, hashCode)))
 				getEventByID(hashCode).removeUser(user);
 	}
 
+	/**
+	 * Removes all users from the event (using removeUserFromEvent method) and then removes/deletes the Event. Notifies Observers
+	 * @param The Event's hashCode to be removed
+	 */
 	public void removeEvent(int hashCode) {
 		Event temp = getEventByID(hashCode);
 		if (temp != null) {
@@ -133,6 +164,10 @@ public class EventManager extends Observable {
 
 		}
 	}
+	/**
+	 * Removes all events and notifies observers
+	 * 
+	 */
 	public void removeAllEvents(){
 		for (LinkedList<Event> list : hMap.values()) {
 		    list.clear();
@@ -141,6 +176,9 @@ public class EventManager extends Observable {
 		setChanged();
 		notifyObservers();
 	}
+	/**
+	 * Removes all Event with dates that are before todays date
+	 */
 	public void removePastEvents(){
 		Iterator<Event> iterator = getEventListIterator();
 		Date date = new Date();
@@ -156,16 +194,25 @@ public class EventManager extends Observable {
 		}
 			
 	}
-
+	/**
+	 * Sorts eventList and returns a Iterator of eventlist
+	 */
 	public Iterator<Event> getEventListIterator() {
 		sortEventList();
 		return eventList.iterator();
 	}
+	/**
+	 * Saves all events to a local Event.sav file
+	 */
 	public void saveEvents()
     {
 		FileHandler.writeFile(eventList);
     }
     
+    	/**
+    	 * Reads from local Event.sav file and adds all events that are not already there. If it doesn't exist throws/catches IOexception
+    	 * Is only called once during initation in constuctor.
+    	 */
     public void readEvents()
     {
     	LinkedList<Event> control = FileHandler.readFile();
