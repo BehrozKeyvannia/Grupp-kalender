9a7ba47077a5cba1bcf9f6371dd984e96999a134
Beyruuz <keyvannia.behroz@gmail.com>
2014-03-26 23:02:16 +0100

    javadoc kommenterad 26/3
    kommenterad javadoc 26/3

--- Grupp-kalender/Com/FileHandler.java
+++ Grupp-kalender/Com/FileHandler.java
@@ -8,14 +8,16 @@ import java.lang.Exception;
 
 /**
 * Filehandler for EventManager
-*
+* This class is responsible for handling the filewriting and filereading for the whole application. This class is only used by 
+* eventManager only. It contains two simple methods which is writing to a file and reading from a file. 
 * @author Akar
 * @version 1.0
 */
 public class FileHandler
 {
     /*
-* A constructor for FileHandler
+* Method writeFile: Accepts a eventList as a parameter as the object that is going to be written to a file named "events.sav". 
+* @param eventList
 */
     public static void writeFile(LinkedList<Event> eventList)
     {
@@ -32,6 +34,11 @@ public class FileHandler
         }
     }
 
+   /*
+* Method readFile: this method reads the file that is written by the "writeFile" method. It returns the eventList that is written to the
+* "events.sav" file. 
+* 
+*/
     public static LinkedList<Event> readFile()
     {
     	LinkedList<Event> readObject = null;
