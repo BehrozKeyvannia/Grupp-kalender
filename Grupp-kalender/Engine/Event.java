7816334dcef7dcd7067a592c548c7335ab1a4bc2
Ashor <user@server.com>
2014-03-27 00:09:56 +0100

    All packages ready for JavaDoc

--- Grupp-kalender/Engine/Event.java
+++ Grupp-kalender/Engine/Event.java
@@ -7,18 +7,16 @@ import java.text.ParseException;
 import java.util.Arrays;
 import java.util.Date;
 
-
 /**
- * Represents a singular event and contains info of the event and the users attending the event.
+ * Represents a singular event and contains info of the event and the users
+ * attending the event.
  * 
  * @author (Ashor, Akar, Ali och Rami)
  * @version 1
  */
 
-public class Event implements Comparable<Event>,Serializable{
-	/**
-	 * 
-	 */
+public class Event implements Comparable<Event>, Serializable {
+
 	private static final long serialVersionUID = 1L;
 	private String userNames;
 	private String title;
@@ -30,7 +28,9 @@ public class Event implements Comparable<Event>,Serializable{
 	private DateFormat formatter;
 
 	/**
-	 * Create an event with parameter values. Formats the yyyy-MM-dd date and HH:mm startTime as a Date object.
+	 * Create an event with parameter values. Formats the yyyy-MM-dd date and
+	 * HH:mm startTime as a Date object.
+	 * 
 	 * @param userName
 	 * @param title
 	 * @param date
@@ -42,9 +42,9 @@ public class Event implements Comparable<Event>,Serializable{
 	 */
 	public Event(String userNames, String title, String date, String startTime,
 			String endTime, String description) {
-		formatter =  new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 		try {
-			this.date2 = (Date) formatter.parse(date+" "+startTime);
+			this.date2 = (Date) formatter.parse(date + " " + startTime);
 		} catch (ParseException e) {
 			System.out.println("Wrong Format on date!");
 		}
@@ -55,147 +55,205 @@ public class Event implements Comparable<Event>,Serializable{
 		this.endTime = endTime;
 		this.description = description;
 	}
+
 	/**
-	 * Create a String array of the userNames splitted by comma ',' and returns it
+	 * Create a String array of the userNames splitted by comma ',' and returns
+	 * it
 	 */
 	public String[] getListOfUsers() {
 		String[] listOfUsers = userNames.split(",");
 		return listOfUsers;
 	}
+
 	/**
-	 * Returns the string array provided by 'getListOfUsers' method as a Arraylist
+	 * Returns the string array provided by 'getListOfUsers' method as a
+	 * Arraylist
 	 */
-	public java.util.List<String> List(){
+	public java.util.List<String> List() {
 		return Arrays.asList(getListOfUsers());
 	}
-	
+
+	/**
+	 * 
+	 * @param newTitle
+	 */
 	public void setTitle(String newTitle) {
 		title = newTitle;
 	}
+
 	/**
 	 * Formats the String newDate to Date object and sets it to the events date
+	 * 
+	 * @param newDate
 	 */
 	public void setDate(String newDate) {
-		formatter =  new SimpleDateFormat("yyyy-MM-dd");
+		formatter = new SimpleDateFormat("yyyy-MM-dd");
 		try {
-			date2 =(Date) formatter.parse(newDate);
+			date2 = (Date) formatter.parse(newDate);
 		} catch (ParseException e) {
 			System.out.println("Wrong Format on Date!");
 		}
-		date=newDate;
+		date = newDate;
 	}
+
 	/**
 	 * Formats the String newStartTime to Date object and sets it to Event date
+	 * 
+	 * @param newStartTime
 	 */
 	public void setStartTime(String newStartTime) {
-		formatter =  new SimpleDateFormat("HH:mm");
+		formatter = new SimpleDateFormat("HH:mm");
 		try {
-			date2 =(Date) formatter.parse(newStartTime);
+			date2 = (Date) formatter.parse(newStartTime);
 		} catch (ParseException e) {
 			System.out.println("Wrong Format on Time!");
 		}
 		startTime = newStartTime;
 	}
 
+	/**
+	 * 
+	 * @param newStartTime
+	 */
 	public void setEndTime(String newStartTime) {
 		endTime = newStartTime;
 	}
 
+	/**
+	 * 
+	 * @param newDescription
+	 */
 	public void setDescription(String newDescription) {
 		description = newDescription;
 	}
 
+	/**
+	 * 
+	 * @return the user name
+	 */
 	public String getUserNames() {
 		return userNames;
 	}
 
+	/**
+	 * 
+	 * @return the title
+	 */
 	public String getTitle() {
 		return title;
 	}
 
+	/**
+	 * 
+	 * @return the date
+	 */
 	public String getDate() {
 		return date;
 	}
 
-	public Date getDate2(){
+	/**
+	 * 
+	 * @return the second date
+	 */
+	public Date getDate2() {
 		return date2;
 	}
 
+	/**
+	 * 
+	 * @return start time
+	 */
 	public String getStartTime() {
 		return startTime;
 	}
 
+	/**
+	 * 
+	 * @return end time
+	 */
 	public String getEndTime() {
 		return endTime;
 	}
-	
+
+	/**
+	 * 
+	 * @return the description
+	 */
 	public String getDescription() {
 		return description;
 	}
+
 	/**
-	 * Returns a String with HTML code for JComponents. /n doesnt work with JComponents. Instead we use <br />
+	 * Returns a String with HTML code for JComponents. /n doesnt work with
+	 * JComponents. Instead we use <br />
 	 */
 	public String getHTMLString() {
 
 		return "<html> " + startTime + "  " + endTime + "<br /> " + title
 				+ "<br /> " + userNames + "</html>";
 	}
+
 	/**
 	 * Removes the User from userNames
+	 * 
+	 * @param userToRemove
 	 */
 	public void removeUser(String userToRemove) {
-		if(List().contains(userToRemove)){
-			if(List().size()==1)
-				userNames = userNames.replace( userToRemove, "");
-			else if(List().indexOf(userToRemove)==List().size()-1)
+		if (List().contains(userToRemove)) {
+			if (List().size() == 1)
+				userNames = userNames.replace(userToRemove, "");
+			else if (List().indexOf(userToRemove) == List().size() - 1)
 				userNames = userNames.replace("," + userToRemove, "");
 			else
 				userNames = userNames.replace(userToRemove + ",", "");
 		}
 	}
+
 	/**
+	 * 
 	 * Adds the user to userNames
+	 * 
+	 * @param userToAdd
 	 */
 	public void addUser(String userToAdd) {
-		if(userNames==null || userNames.equals(""))
-			userNames=userToAdd;
+		if (userNames == null || userNames.equals(""))
+			userNames = userToAdd;
 		else
 			userNames += ("," + userToAdd);
 	}
-	
+
 	/**
-	 * Compares current and parameter Event by Dates. If o is past <0 is returned. If o is equal 0 is returned. If o is future date >0 is returned.
+	 * Compares current and parameter Event by Dates. If o is past <0 is
+	 * returned. If o is equal 0 is returned. If o is future date >0 is
+	 * returned.
 	 */
 	@Override
 	public int compareTo(Event o) {
 		return getDate2().compareTo(o.getDate2());
 	}
+
 	@Override
 	public int hashCode() {
-        return title.hashCode()+
-            date.hashCode()+
-            startTime.hashCode()+
-            endTime.hashCode()+
-            description.hashCode();
-    }
-    /**
-     * Title, date, startTime, endTime and descprition are compared.
-     */
-	@Override
-    public boolean equals(Object obj) {
-        if (obj == null)
-            return false;
-        if (obj == this)
-            return true;
-        if (!(obj instanceof Event))
-            return false;
+		return title.hashCode() + date.hashCode() + startTime.hashCode()
+				+ endTime.hashCode() + description.hashCode();
+	}
 
-        Event rhs = (Event) obj;
-        return 
-            (title.equals(rhs.title)&&
-            date.equals(rhs.date)&&
-            startTime.equals(rhs.startTime)&&
-            endTime.equals(rhs.endTime)&&
-            description.equals(rhs.description));
-    }
+	/**
+	 * Title, date, startTime, endTime and descprition are compared.
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null)
+			return false;
+		if (obj == this)
+			return true;
+		if (!(obj instanceof Event))
+			return false;
+
+		Event rhs = (Event) obj;
+		return (title.equals(rhs.title) && date.equals(rhs.date)
+				&& startTime.equals(rhs.startTime)
+				&& endTime.equals(rhs.endTime) && description
+					.equals(rhs.description));
+	}
 }
--- Grupp-kalender/Engine/EventManager.java
+++ Grupp-kalender/Engine/EventManager.java
@@ -6,12 +6,12 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Observable;
-
 import Com.FileHandler;
 
 /**
- * Manages the events and handles every operation that has to do with Event outside of the Event class.
- * This class creates, finds, saves, sorts, adds, organizes,and removes Events.
+ * Manages the events and handles every operation that has to do with Event
+ * outside of the Event class. This class creates, finds, saves, sorts, adds,
+ * organizes,and removes Events.
  * 
  * @author (Ashor, Akar, Ali och Rami)
  * @version 1
@@ -40,7 +40,13 @@ public class EventManager extends Observable {
 	/**
 	 * Creates and returns a Event with the specified info
 	 * 
+	 * @param userNames
+	 * @param title
+	 * @param date
+	 * @param startTime
+	 * @param endTime
 	 * @param description
+	 * @return the event
 	 */
 	public Event createNewEvent(String userNames, String title, String date,
 			String startTime, String endTime, String description) {
@@ -61,18 +67,21 @@ public class EventManager extends Observable {
 			addEvent(tempIterator.next());
 		}
 	}
+
 	/**
-	* Sorts all events in Eventlist in chronlogical order.
-	*/
+	 * Sorts all events in Event list in chronological order.
+	 */
 	public void sortEventList() {
 		Collections.sort(eventList);
 	}
-	
+
 	/**
-	 *
-	 * Adds a event if it isn't already in eventList and adds the event in all the participants list in hMap. Notifies Observers
+	 * 
+	 * Adds a event if it isn't already in eventList and adds the event in all
+	 * the participants list in hMap. Notifies Observers
+	 * 
 	 * @param event
-	 * 		The event that will be added
+	 *            The event that will be added
 	 * 
 	 */
 	public Event addEvent(Event event) {
@@ -95,31 +104,35 @@ public class EventManager extends Observable {
 	/**
 	 * 
 	 * @param user
-	 *            Vilken användares lista som kmr sökas
-	 * @param id
-	 *            Eventets ID
-	 * @return Eventet som söks
+	 * @param hashCode
+	 * @return the event
 	 */
 	public Event getEvent(String user, int hashCode) {
-		// Gå igenom användares lista o hitta rätt event id
 		return searchEvent(hMap.get(user), hashCode);
 
 	}
-	
+
 	/**
-	 * Searches for an Event in eventList and returnes the event with the specified hashCode
-	 * @param hashCode hashCode to be searched
+	 * Searches for an Event in eventList and returnes the event with the
+	 * specified hashCode
+	 * 
+	 * @param hashCode
+	 *            hashCode to be searched
 	 * @return Event If Event found it is returned else returns null.
 	 * 
 	 */
-	
 	public Event getEventByID(int hashCode) {
 		return searchEvent(eventList, hashCode);
 	}
+
 	/**
-	 * Searches through a list(param) for a Event with the specified hashCode(param)
-	 * @param list List to be searched
-	 * @param hashCode hashCode to search after.
+	 * Searches through a list(param) for a Event with the specified
+	 * hashCode(param)
+	 * 
+	 * @param list
+	 *            List to be searched
+	 * @param hashCode
+	 *            hashCode to search after.
 	 * 
 	 * @return Event if found else null.
 	 */
@@ -134,10 +147,15 @@ public class EventManager extends Observable {
 		// Inget event hittas returnas null
 		return null;
 	}
+
 	/**
-	 * Removes the EventReference with the specified hashCode in a user list and removes the users name in the event's userNames.
-	 * @param user user to remove
-	 * @param hashCode Events hashcode which the user is removed from.
+	 * Removes the EventReference with the specified hashCode in a user list and
+	 * removes the users name in the event's userNames.
+	 * 
+	 * @param user
+	 *            user to remove
+	 * @param hashCode
+	 *            Events hashcode which the user is removed from.
 	 * 
 	 */
 	public void removeUserFromEvent(String user, int hashCode) {
@@ -147,8 +165,11 @@ public class EventManager extends Observable {
 	}
 
 	/**
-	 * Removes all users from the event (using removeUserFromEvent method) and then removes/deletes the Event. Notifies Observers
-	 * @param The Event's hashCode to be removed
+	 * Removes all users from the event (using removeUserFromEvent method) and
+	 * then removes/deletes the Event. Notifies Observers
+	 * 
+	 * @param The
+	 *            Event's hashCode to be removed
 	 */
 	public void removeEvent(int hashCode) {
 		Event temp = getEventByID(hashCode);
@@ -165,36 +186,39 @@ public class EventManager extends Observable {
 
 		}
 	}
+
 	/**
 	 * Removes all events and notifies observers
 	 * 
 	 */
-	public void removeAllEvents(){
+	public void removeAllEvents() {
 		for (LinkedList<Event> list : hMap.values()) {
-		    list.clear();
+			list.clear();
 		}
 		eventList.clear();
 		setChanged();
 		notifyObservers();
 	}
+
 	/**
 	 * Removes all Event with dates that are before todays date
 	 */
-	public void removePastEvents(){
+	public void removePastEvents() {
 		Iterator<Event> iterator = getEventListIterator();
 		Date date = new Date();
-		while(iterator.hasNext()){
+		while (iterator.hasNext()) {
 			Event temp = iterator.next();
-			if(date.compareTo(temp.getDate2())<0)
+			if (date.compareTo(temp.getDate2()) < 0)
 				break;
 			String[] x = temp.getListOfUsers();
-			for(int i =0;i<x.length;i++)
-				removeUserFromEvent(x[i],temp.hashCode());
+			for (int i = 0; i < x.length; i++)
+				removeUserFromEvent(x[i], temp.hashCode());
 			iterator.remove();
-				
+
 		}
-			
+
 	}
+
 	/**
 	 * Sorts eventList and returns a Iterator of eventlist
 	 */
@@ -202,22 +226,23 @@ public class EventManager extends Observable {
 		sortEventList();
 		return eventList.iterator();
 	}
+
 	/**
 	 * Saves all events to a local Event.sav file
 	 */
-	public void saveEvents()
-    {
+	public void saveEvents() {
 		FileHandler.writeFile(eventList);
-    }
-    
-    	/**
-    	 * Reads from local Event.sav file and adds all events that are not already there. If it doesn't exist throws/catches IOexception
-    	 * Is only called once during initation in constuctor.
-    	 */
-    public void readEvents()
-    {
-    	LinkedList<Event> control = FileHandler.readFile();
-    	if(control!=null)
-    		addEventList(control); //FÅR ENDAST ANVÄNDAS VID APPLIKATIONENS START
-    }
+	}
+
+	/**
+	 * Reads from local Event.sav file and adds all events that are not already
+	 * there. If it doesn't exist throws/catches IOexception Is only called once
+	 * during initation in constuctor.
+	 */
+	public void readEvents() {
+		LinkedList<Event> control = FileHandler.readFile();
+		if (control != null)
+			addEventList(control); // FÅR ENDAST ANVÄNDAS VID APPLIKATIONENS
+									// START
+	}
 }
